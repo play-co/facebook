3a0917132819967ca11655d2e7c5ce8a34c18994
Martin Hunt <martin@gameclosure.com>
2015-06-15 09:50:46 -0700

    web sdk switch to "sdk.js" from "all.js"
     - update for fb-recommended switch to sdk.js
     - use sdk/debug.js for simulator builds
        - unminified facebook js sdk source
        - extra logging for warnings and errors
     - adds #fb-root div, required for web sdk

--- build/index.js
+++ build/index.js
@@ -12,13 +12,16 @@ exports.onBeforeBuild = function (api, app, config, cb) {
     }
 
     if (config.browser.bodyHTML) {
+      var filename = config.isSimulated ? 'sdk/debug.js' : 'sdk.js';
+
       config.browser.bodyHTML.push(
+        '<div id="fb-root"></div>',
         '<script>',
           '(function(d, s, id){',
              'var js, fjs = d.getElementsByTagName(s)[0];',
              'if (d.getElementById(id)) {return;}',
              'js = d.createElement(s); js.id = id;',
-             'js.src = "//connect.facebook.net/en_US/all.js";',
+             'js.src = "//connect.facebook.net/en_US/' + filename + '";',
              'fjs.parentNode.insertBefore(js, fjs);',
            '}(document, "script", "facebook-jssdk"));',
         '</script>'
